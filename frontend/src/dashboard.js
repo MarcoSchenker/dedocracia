6717c19b1ce3a33c9a630ab3f27bcd2c17e94bb7
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-05-22 12:30:11 -0300

    cambios env

--- frontend/src/dashboard.js
+++ frontend/src/dashboard.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { Users, Activity, TrendingUp, TrendingDown, Plus } from 'lucide-react';
 
